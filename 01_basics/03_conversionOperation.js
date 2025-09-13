19052d2dfc6c196c4aebb67abcb3651ae5f57757
Ninlord <tanaych8@gmail.com>
2025-09-13 13:54:18 +0530

    lecture6

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "10"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1, false => 0
+
+let isLoggedIn = 1
+let bolleanisLoggedIn = Boolean(isLoggedIn)
+
+console.log(bolleanisLoggedIn)
+
+// 1=> true and 0 => false
+// "" false
+// "xyz" true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber, "=>", stringNumber);
